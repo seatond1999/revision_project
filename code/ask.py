9d9d6c18f70e2753d005931491b3148200dcf54b
daniel seaton <danseaton@kubrickgroup.com>
2023-11-21 17:09:03 +0000

    adding all

---
+++ code/ask.py
@@ -0,0 +1,179 @@
+# %% --------------------------------------------------------------------------
+
+
+#!pip install bitsandbytes
+#!pip install -q -U git+https://github.com/huggingface/transformers.git
+#!pip install -q -U git+https://github.com/huggingface/accelerate.git
+#!pip install -q -U git+https://github.com/huggingface/peft.git
+#!pip install -qqq torch
+#!pip3 install auto-gptq --extra-index-url https://huggingface.github.io/autogptq-index/whl/cu118/
+#!pip install optimum
+#!pip install trl
+#!pip install py7zr
+
+#oneliner...
+# pip install bitsandbytes git+https://github.com/huggingface/transformers.git git+https://github.com/huggingface/accelerate.git git+https://github.com/huggingface/peft.git torch auto-gptq --extra-index-url https://huggingface.github.io/autogptq-index/whl/cu118/ optimum trl py7zr
+#pip3 install tensorboard scipy bitsandbytes git+https://github.com/huggingface/transformers.git git+https://github.com/huggingface/accelerate.git git+https://github.com/huggingface/peft.git auto-gptq optimum trl py7zr
+# %% --------------------------------------------------------------------------
+import pandas as pd
+from datasets import load_dataset, Dataset
+from transformers import (
+    AutoModelForCausalLM,
+    AutoTokenizer,
+    GPTQConfig,
+    TrainingArguments,
+    TrainerCallback,
+)
+from peft import LoraConfig, prepare_model_for_kbit_training, get_peft_model
+from trl import SFTTrainer
+import os
+import tempfile
+from huggingface_hub import login
+from datetime import datetime as dt
+import torch
+# -----------------------------------------------------------------------------
+
+## timer
+def timer(func):
+    def do(x):
+        before = dt.now()
+        perform = func(x)
+        after = dt.now()
+        print(after-before)
+        return perform
+    return do
+
+# %%
+token = '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'
+login(token = token)
+
+# preprocessing:
+
+
+def prep_data():
+    data = pd.read_csv(r'../ready_data.csv')
+    # context had disctionary, only interested in the 'contexts' key
+
+    # create data for asking Qs (aq)
+    data_aq = pd.DataFrame(
+        {
+            "content": data.apply(
+                lambda x: "###human: Ask me a questions about:\n"
+                + x["full_context"]
+                + "\n###Response:\n"
+                + x["full_questions"],
+                axis=1,
+            )
+        }
+    )
+    data_aq = Dataset.from_pandas(data_aq)
+    data_aq = data_aq.train_test_split(test_size=0.016)
+    return data_aq
+
+@timer
+def finetune(data):
+    train_data = data["train"]
+    test_data = data["test"]
+
+    ##load model and tokenizer
+    model_id = "TheBloke/Mistral-7B-v0.1-GPTQ"
+
+    tokenizer = AutoTokenizer.from_pretrained(model_id)
+    tokenizer.pad_token = tokenizer.eos_token
+
+    quantization_config_loading = GPTQConfig(
+        bits=4, disable_exllama=True, tokenizer=tokenizer
+    )
+    model = AutoModelForCausalLM.from_pretrained(
+        model_id, quantization_config=quantization_config_loading, device_map="auto"
+    )
+
+    ##finetune:
+
+    print(model)
+    model.config.use_cache = False  # wont store intermediate states
+    model.config.pretraining_tp = 1  # to replicate pre-training performance
+    model.gradient_checkpointing_enable()  # compramise between forgetting activation states and remmebering them for backpropagation. Trade off computation time for GPU memory
+    model = prepare_model_for_kbit_training(model)
+    peft_config = LoraConfig(
+        r=16,
+        lora_alpha=16,
+        lora_dropout=0.05,
+        bias="none",
+        task_type="CAUSAL_LM",
+        target_modules=["q_proj", "v_proj"],
+    )
+    model = get_peft_model(model, peft_config)
+    name = "mist_question_asking"
+    training_arguments = TrainingArguments(
+        output_dir=name,
+        per_device_train_batch_size=8,
+        gradient_accumulation_steps=1,
+        optim="paged_adamw_32bit",
+        learning_rate=2e-4,
+        lr_scheduler_type="cosine",
+        save_strategy="epoch",  # so do we need the whole PeftSavingCallback function? maybe try withput and run the trainer(from last check=true)
+        logging_steps=100,
+        num_train_epochs=1,
+        max_steps=250,
+        fp16=True,
+        #push_to_hub=True,
+        report_to=["tensorboard"],
+    )
+
+    # if checkpint doesnt work, might have to do save_steps=20 for example in the training argumnet above
+
+    ############## checkpoint ##############################
+    class PeftSavingCallback(TrainerCallback):
+        def on_save(self, args, state, control, **kwargs):
+            checkpoint_path = os.path.join(
+                args.output_dir, f"checkpoint-{state.global_step}"
+            )
+            kwargs["model"].save_pretrained(checkpoint_path)
+
+            if "pytorch_model.bin" in os.listdir(checkpoint_path):
+                os.remove(os.path.join(checkpoint_path, "pytorch_model.bin"))
+
+    callbacks = [PeftSavingCallback()]
+
+    trainer = SFTTrainer(
+        model=model,
+        train_dataset=train_data,
+        eval_dataset=test_data,
+        peft_config=peft_config,
+        dataset_text_field="content",
+        args=training_arguments,
+        tokenizer=tokenizer,
+        callbacks=callbacks,
+        packing=False,
+    )
+
+    ###################################################
+
+    ########## set up tensorboard #####################
+    tmpdir = tempfile.TemporaryDirectory()
+    local_training_root = tmpdir.name
+
+    loc_checkpoint_path = os.path.join(local_training_root, name)
+    tensorboard_display_dir = f"{loc_checkpoint_path}/runs"
+
+    %load_ext tensorboard
+    %tensorboard --logdir f'{tensorboard_display_dir}'
+
+    #########################################################
+
+    trainer.train(resume_from_checkpoint=True)
+    #trainer.state.log_history()
+    #trainer.save_model()
+    #trainer.push_to_hub() #un hash when want to send final model to hub
+    return trainer
+
+
+# resume_from_checkpoint (str or bool, optional) — If a str, local path to a saved checkpoint as saved by a previous instance of Trainer. If a bool and equals True, load the last checkpoint in args.output_dir as saved by a previous instance of Trainer. If present, training will resume from the model/optimizer/scheduler states loaded here.
+
+
+
+# %% --------------------------------------------------------------------------
+# RUN!
+if __name__ == '__main__':
+    trainer_obj = finetune(prep_data())
---
+++ code/augment.py
@@ -0,0 +1,169 @@
+# %% --------------------------------------------------------------------------
+import pandas as pd
+from datasets import load_dataset, Dataset
+from openai import OpenAI
+import os
+import time
+import matplotlib.pyplot as plt
+
+
+# -----------------------------------------------------------------------------
+# %% -------------------------------------------------------------------------# -----------------------------------------------------------------------------
+def load_data():
+    dataset = load_dataset(
+        "pubmed_qa", "pqa_labeled"
+    )  # this is the 1000 'expertly labelled' dataset (can choose from "pqa_artificial", "pqa_labeled", "pqa_unlabeled")
+    data = dataset["train"].to_pandas()[["question", "context", "long_answer"]]
+    data["context"] = data["context"].apply(lambda x: "".join(x["contexts"]))
+    extra_data = data.iloc[664:]
+    data = data.iloc[0:664, :]  # as dont have many credits on
+    return data, extra_data
+
+
+if __name__ == "__main__":
+    data = load_data()
+
+
+# %% --------------------------------------------------------------------------
+# function fo enrivching data - add line which puts it on to table and joins with other question
+def gpt_enrich_data(data_inp, start_row=0, end_row=None):
+    end_row = len(data_inp) if end_row is None else end_row
+    # create prompt for GPT 3.5:
+    data_to_enrich = pd.DataFrame(
+        {
+            "prompt": data_inp.apply(
+                lambda x: """You are an examiner who will be provided with context and an example question based on the context which will be separated by the delimiter '###'. You must ask a new question which can be answered using only the context provided.\nContext: ### """
+                + x["context"]
+                + " ###\nExample question based on the context: ### "
+                + x["question"]
+                + " ###\nQuestion based on the context: ### ",
+                axis=1,
+            ),
+        }
+    )
+    # pass each prompt into GPT and put return in new column:
+    data_to_enrich = data_to_enrich.iloc[
+        start_row:end_row
+    ]  # i dont pass all due to open AI limits
+    client = OpenAI()
+    new_questions = []
+    for i in range(0, len(data_to_enrich)):
+        print(i)
+        new_questions.append(
+            client.chat.completions.create(
+                model="gpt-3.5-turbo",
+                messages=[{"role": "user", "content": data_to_enrich.iloc[i, 0]}],
+            )
+            .choices[0]
+            .message.content
+        )
+        print(i)
+        time.sleep(60) if (i + 1) % 3 == 0 else None
+
+    enriched_data = data_inp
+    enriched_data.loc[start_row : end_row - 1, "gpt_question"] = new_questions
+    return enriched_data
+
+
+if __name__ == "__main__":
+    enriched_data = gpt_enrich_data(data[0])
+# if doing in batches of 200 would do this:
+# enriched_data = gpt_enrich_data(enriched_data, 200,401) etc spaced 1 day apart lol
+
+# enriched_data.to_csv(r'../enriched_data.csv')
+
+# %% --------------------------------------------------------------------------
+## add mulitQ
+
+
+# -----------------------------------------------------------------------------
+
+
+# %% --------------------------------------------------------------------------
+def create_multiq_data(data_unprepared, single_qs):
+    # prep single question data
+    single_qs = single_qs[["context", "question"]]
+    single_qs.rename(
+        columns={"context": "context", "question": "combined_questions"}, inplace=True
+    )
+    single_qs["context_length"] = single_qs.apply(
+        lambda x: len(x["context"].split()), axis=1
+    )
+    single_qs = single_qs.sort_values(by="context_length", ascending=True)
+    single_qs.reset_index(inplace=True)
+    single_qs = single_qs.head(166)
+    ##
+    df = data_unprepared
+    df["combined_questions"] = df["question"] + "\n" + df["gpt_question"]
+    df["context_length"] = df.apply(lambda x: len(x["context"].split()), axis=1)
+    df = df.sort_values(by="context_length", ascending=False)
+    df.reset_index(inplace=True)
+    # create quad Qs with longest contexts:
+    first_quart = df.loc[0:165, ["context", "combined_questions"]]
+    first_quart.reset_index(inplace=True)
+    second_quart = df.loc[166:331, ["context", "combined_questions"]][
+        ::-1
+    ]  # can tidy later
+    second_quart.reset_index(inplace=True)
+    quad_qs = pd.concat([first_quart, second_quart], axis=1)
+    quad_qs["full_context"] = quad_qs.iloc[:, 1] + "\n" + quad_qs.iloc[:, 4]
+    quad_qs["full_questions"] = quad_qs.iloc[:, 2] + "\n" + quad_qs.iloc[:, 5]
+    # create triple Qs:
+    third_quart = df.loc[332:497, ["context", "combined_questions"]]
+    third_quart.reset_index(inplace=True)
+    triple_qs = pd.concat([third_quart, single_qs], axis=1)
+    triple_qs["full_context"] = triple_qs.iloc[:, 1] + "\n" + triple_qs.iloc[:, 4]
+    triple_qs["full_questions"] = triple_qs.iloc[:, 2] + "\n" + triple_qs.iloc[:, 5]
+    # create double Qs
+    forth_quart = df.loc[498:663, ["context", "combined_questions"]]
+    forth_quart.reset_index(inplace=True)
+    double_qs = forth_quart
+    double_qs.rename(
+        columns={"context": "full_context", "combined_questions": "full_questions"},
+        inplace=True,
+    )
+    # put together!
+    combined = pd.concat(
+        [
+            quad_qs[["full_context", "full_questions"]],
+            triple_qs[["full_context", "full_questions"]],
+            double_qs[["full_context", "full_questions"]],
+        ]
+    )
+    combined["context_length"] = combined.apply(
+        lambda x: len(x["full_context"].split()), axis=1
+    )
+    combined.reset_index(inplace=True)
+    combined.drop(columns=["index"], inplace=True)
+    return combined
+
+
+if __name__ == "__main__":
+    enriched_data = pd.read_csv(r"../enriched_data.csv")
+    enriched_data.drop(columns=["Unnamed: 0"], inplace=True)
+    extra_data = data[1]
+
+    ready_data = create_multiq_data(enriched_data, extra_data)
+# -----------------------------------------------------------------------------
+
+# %% --------------------------------------------------------------------------
+
+
+# context length dist per group
+def context_length_dist():
+    print(plt.hist(ready_data.iloc[0:166, 2]))
+    print(plt.hist(ready_data.iloc[166:332, 2]))
+    print(plt.hist(ready_data.iloc[332:498, 2]))
+
+
+if __name__ == "__main__":
+    context_length_dist()
+# -----------------------------------------------------------------------------
+
+# %% --------------------------------------------------------------------------
+if __name__ == "__main__":
+    ready_data.to_csv(r"../ready_data.csv")
+# -----------------------------------------------------------------------------
+
+
+# %%
