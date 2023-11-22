c1877355b81a178c2e61fbe8ac59be23616fbc01
daniel seaton <danseaton@kubrickgroup.com>
2023-11-22 11:12:11 +0000

    d

--- code/ask.py
+++ code/ask.py
@@ -11,9 +11,9 @@
 #!pip install trl
 #!pip install py7zr
 
-#oneliner...
+# oneliner...
 # pip install bitsandbytes git+https://github.com/huggingface/transformers.git git+https://github.com/huggingface/accelerate.git git+https://github.com/huggingface/peft.git torch auto-gptq --extra-index-url https://huggingface.github.io/autogptq-index/whl/cu118/ optimum trl py7zr
-#pip3 install tensorboard scipy bitsandbytes git+https://github.com/huggingface/transformers.git git+https://github.com/huggingface/accelerate.git git+https://github.com/huggingface/peft.git auto-gptq optimum trl py7zr
+# pip3 install tensorboard scipy bitsandbytes git+https://github.com/huggingface/transformers.git git+https://github.com/huggingface/accelerate.git git+https://github.com/huggingface/peft.git auto-gptq optimum trl py7zr
 # %% --------------------------------------------------------------------------
 import pandas as pd
 from datasets import load_dataset, Dataset
@@ -31,7 +31,9 @@ import tempfile
 from huggingface_hub import login
 from datetime import datetime as dt
 import torch
+
 # -----------------------------------------------------------------------------
+
 
 ## timer
 def timer(func):
@@ -39,19 +41,21 @@ def timer(func):
         before = dt.now()
         perform = func(x)
         after = dt.now()
-        print(after-before)
+        print(after - before)
         return perform
+
     return do
 
+
 # %%
-token = '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'
-login(token = token)
+token = "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"
+login(token=token)
 
 # preprocessing:
 
 
 def prep_data():
-    data = pd.read_csv(r'../ready_data.csv')
+    data = pd.read_csv(r"../ready_data.csv")
     # context had disctionary, only interested in the 'contexts' key
 
     # create data for asking Qs (aq)
@@ -69,6 +73,7 @@ def prep_data():
     data_aq = Dataset.from_pandas(data_aq)
     data_aq = data_aq.train_test_split(test_size=0.016)
     return data_aq
+
 
 @timer
 def finetune(data):
@@ -110,14 +115,14 @@ def finetune(data):
         per_device_train_batch_size=8,
         gradient_accumulation_steps=1,
         optim="paged_adamw_32bit",
-        learning_rate=2e-4,
+        learning_rate=3e-4,
         lr_scheduler_type="cosine",
         save_strategy="epoch",  # so do we need the whole PeftSavingCallback function? maybe try withput and run the trainer(from last check=true)
         logging_steps=100,
         num_train_epochs=1,
         max_steps=250,
         fp16=True,
-        #push_to_hub=True,
+        # push_to_hub=True,
         report_to=["tensorboard"],
     )
 
@@ -151,31 +156,40 @@ def finetune(data):
     ###################################################
 
     ########## set up tensorboard #####################
-    #tmpdir = tempfile.TemporaryDirectory()
-    #local_training_root = tmpdir.name
+    # tmpdir = tempfile.TemporaryDirectory()
+    # local_training_root = tmpdir.name
 
-    #loc_checkpoint_path = os.path.join(local_training_root, name)
-    #tensorboard_display_dir = f"{loc_checkpoint_path}/runs"
+    # loc_checkpoint_path = os.path.join(local_training_root, name)
+    # tensorboard_display_dir = f"{loc_checkpoint_path}/runs"
 
-    #%load_ext tensorboard
-    #%tensorboard --logdir f'{tensorboard_display_dir}'
+    # %load_ext tensorboard
+    # %tensorboard --logdir f'{tensorboard_display_dir}'
 
     #########################################################
 
-    trainer.train(resume_from_checkpoint=True)
-    #trainer.state.log_history()
-    #trainer.save_model()
-    #trainer.push_to_hub() #un hash when want to send final model to hub
+    # trainer.train(resume_from_checkpoint=True) #only use if going from checkpoint
+    trainer.train()
+    # trainer.state.log_history()
+    # trainer.save_model()
+    # trainer.push_to_hub() #un hash when want to send final model to hub
     return trainer
 
 
 # resume_from_checkpoint (str or bool, optional) — If a str, local path to a saved checkpoint as saved by a previous instance of Trainer. If a bool and equals True, load the last checkpoint in args.output_dir as saved by a previous instance of Trainer. If present, training will resume from the model/optimizer/scheduler states loaded here.
 
 
-
 # %% --------------------------------------------------------------------------
 # RUN!
-if __name__ == '__main__':
+if __name__ == "__main__":
     trainer_obj = finetune(prep_data())
 
-    #see what changes made on VM then change git then pull to VM.
+    # see what changes made on VM then change git then pull to VM.
+
+
+# %% --------------------------------------------------------------------------
+import os,sys
+url = 'https://github.com/seatond1999/revision_project/tree/developing/code/mist_question_asking/checkpoint-62'
+os.system('gitdir'+url)
+# -----------------------------------------------------------------------------
+
+# %%
