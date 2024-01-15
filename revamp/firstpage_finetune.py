1fc25b4475522cb6c5469dfe737e3c3d40eb6e16
daniel seaton <danseaton@kubrickgroup.com>
2024-01-15 17:43:34 +0000

    f

--- revamp/firstpage_finetune.py
+++ revamp/firstpage_finetune.py
@@ -40,18 +40,28 @@ token = "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"
 login(token=token)
 
 def prep_data():
-    data = pd.read_json(r"synthetic_data_newest.json")
+    data = pd.read_json(r"page_identification_data.json")
     data_aq = pd.DataFrame(
         {
             "content": data.apply(
-                lambda x: f"""[INST]You must split text up into subsections and add informative titles for each subsection.
-Each subsection must be in paragraph form and all information should be included from the original text.
-You will be penalized for removing information from the original text.
-Mark each title you create by adding the symbols "@@@" before each title and placing the title on its own line.
-An example subsection format is "@@@title \n content", where you should add the subsection title and content.
-This is the text:
-### {x['pages']} ### [/INST]
-Output: {x['sums']} </s>""",
+                lambda x: f"""<s>[INST] @@@ Instructions:
+You are an assisstant who must classify whether a string from a page of a pdf book corresponds to the first page of a given chapter in that book.
+You will be given the string and also given the chapter title.
+The first page of a chapter usually contains the name of the chapter towards the start of the string.
+The first word of you answer must be "Yes" or "No"
+You must reply "yes" if the string is the first page of the given chapter, and "no" if it is not the first page of the given chapter.
+
+@@@ Example:
+User: The given chapter title is ### The Chemistry Of Life ### and the string is: ### 'CHEMISTRY FOR BIOLOGISTS 61A.1 THE CHEMISTRY OF LIFE\nTHE CHEMISTRY OF WATER\nAll reactions in living cells take place in water. Without water, \nsubstances could not move around the body. Water is one of  the reactants in the process of  photosynthesis, on which almost all life depends (see fig E). Understanding the properties of  water will help you understand many key systems in living organisms. \nWater is also a major habitat – it supports more life than any other \npart of  the planet.\n▲ fig E  W ater is vital for life on Earth in many different ways – in a desert, \nthe smallest amount of water allows plants to grow.\nThe simple chemical formula of  water is H2O. This tells us that \ntwo atoms of  hydrogen are joined to one atom of  oxygen to make \nup each water molecule. However, because the electrons are held closer to the oxygen atom than to the hydrogen atoms, water is a polar molecule (see fig F).\n104.5°Oδ2\nHδ1Hδ1\n▲ fig F  A model of a w ater molecule showing dipoles.\nOne major effect of  this polarity is that water molecules form hydrogen  bonds. The slightly negative oxygen atom of  one water \nmolecule will attract the slightly positive hydrogen atoms of  other water molecules in a weak electrostatic attraction called a hydrogen \nbond. Each individual hydrogen bond is weak but there are many of  them so the molecules of  water ‘stick together’ more than you might expect (see fig G). Water has relatively high melting and boiling points compared with other substances that have molecules of  a similar size because it takes a lot of  energy to break all the hydrogen bonds that hold the molecules together. Hydrogen bonds are important in protein structure (see Sections 1A.5 and 2B.1) \nand in the structure and functioning of  DNA (see Section 2B.3).Oδ2\nOδ2\nHδ1Hδ1Hδ1Hδ1\nHδ1Oδ2Oδ2\nOδ2Hδ1\nHδ1\nHδ1Hδ1Hδ1\n▲ fig G  Hydr ogen bonding in water molecules, based on attraction \nbetween positive and negative dipoles.\nTHE IMPORTANCE OF WATER\nThe properties of  water make it very important in biological \nsystems for many reasons.\n •W ater is a polar solvent. Because it is a polar molecule, many ionic \nsubstances like sodium chloride will dissolve in it (see fig H).  \nMany covalently bonded substances are also polar and will dissolve in water, but often do not dissolve in other covalently bonded solvents such as ethanol. Water also carries other substances, such as starch. As a result, most of  the chemical reactions within cells occur in water (in aqueous solution).\nsodium and chloride ionsin solution in water\nsalt and water mixed\nsodium chloride\nNaClionic bond sodium ionchlorideionδ1 chargeson hydrogenin water areattracted tonegativechloride ion\nδ2\n charges\non oxygenin water ar\ne\nattracted tothe positivesodium ionH\nH\nHHH\nH HO\nOO\nCl2\nCl2\nCl2Cl2\nCl2\nNa1OH\nCl2\nNa1Na1\nCl2Cl2\nCl2\nCl2Cl2\nNa1Na1Na1 Na1\nNa1Na1Na1\n▲ fig H  A model of sodium chloride dissolving in water as a result of the \ninteractions between the charges on sodium and chloride ions and the dipoles of the water molecules.\nUncorrected proof, all content subject to change at publisher discretion. Not for resale, circulation or distribution in whole or in part. ©Pearson 2018' ###
+Assisstant: "Yes"
+
+@@@ Example:
+User: The given chapter title is ### Preparing For Your Exams ### and the string is: ### 'xASSESSMENT OVERVIEW\nPAPER / UNIT 1PERCENTAGE \nOF IASPERCENTAGE OF IALMARK TIME AVAILABILITY\nMOLECULES, DIET, TRANSPORT AND \nHEALTH \nWritten examination\nPaper code \nWBI11/01\nExternally set and marked by \nPearson Edexcel\nSingle tier of entry40% 20% 80 1 hour  \n30 minutesJanuary, June and October\nFirst assessment : January 2019\nPAPER / UNIT 2PERCENTAGE \nOF IASPERCENTAGE OF IALMARK TIME AVAILABILITY\nCELLS, DEVELOPMENT, BIODIVERSITY \nAND CONSERVATION\nWritten examination\nPaper code \nWBI12/01\nExternally set and marked by \nPearson Edexcel\nSingle tier of entry40% 20% 80 1 hour  \n30 minutesJanuary, June and October\nFirst assessment : June 2019\nPAPER / UNIT 3PERCENTAGE \nOF IASPERCENTAGE OF IALMARK TIME AVAILABILITY\nPRACTICAL SKILLS IN BIOLOGY 1   \nWritten examination\nPaper code \nWBI13/01\nExternally set and marked by \nPearson Edexcel\nSingle tier of entry20% 10% 50 1 hour  \n20 minutesJanuary, June and October\nFirst assessment : June 2019ASSESSMENT OVERVIEW\nThe following tables give an overview of the assessment for Pearson Edexcel International Advanced Subsidiary course \nin Biology. You should study this information closely to help ensure that you are fully prepared for this course and know exactly what to expect in each part of the examination. More information about this qualification, and about the question types in the different papers, can be found on page 302 of this book.\nUncorrected proof, all content subject to change at publisher discretion. Not for resale, circulation or distribution in whole or in part. ©Pearson 2018' ###
+Assisstant: "No"
+
+@@@ Question:
+User: The given chapter title: ### {x['chapter_title']} ### This is the string: ### {x['page']} ### [/INST]
+Assisstant: {x['label']}""",
                 axis=1,
             )
         }
@@ -200,7 +210,9 @@ def finetune(data, r, lora_alpha, lr, epochs, target_modules,batch_s,gradacc):
 # import os
 # os.environ['PYTORCH_CUDA_ALLOC_CONF'] = 'max_split_size_mb=10'
 if __name__ == "__main__":
-    trainer_obj = finetune(prep_data(), 64, 128, 2.2e-5, 2, ["q_proj", "v_proj","o_proj","k_proj","up_proj","down_proj","gate_proj"],1,6)
+    trainer_obj = finetune(prep_data(), 64, 128, 2.2e-5, 2, ["q_proj", "v_proj","o_proj","k_proj","up_proj","down_proj","gate_proj"],1,4
+                           
+                           )
 
 #,"gate_proj"
 #,"gate_proj","up_proj","down_proj"
@@ -214,21 +226,3 @@ trainer_obj.save_model()
 # %% --------------------------------------------------------------------------
 
 # %% --------------------------------------------------------------------------
-
-
-# %%
-#see max sequence length of input
-hi = 0
-if hi == 1:
-    from transformers import AutoTokenizer
-    model_id = "TheBloke/Mistral-7B-v0.1-GPTQ"
-
-    tokenizer = AutoTokenizer.from_pretrained(
-        model_id, use_fast=False, add_eos_token=True)
-    
-    lens = []
-    for i in prep_data()['train']['content']:
-        lens.append(len(tokenizer(i)['input_ids']))
-
-    print(max(lens))
-    # %%
---
+++ revamp/firstpage_make_data.py
@@ -0,0 +1,534 @@
+# %% --------------------------------------------------------------------------
+from PyPDF2 import PdfReader
+import pandas as pd
+
+# -----------------------------------------------------------------------------
+
+# %% --------------------------------------------------------------------------
+# aqa biology
+[]
+yes = [
+    #yes.... add 5
+    2,
+    6,#Lipids
+    8, #Proteins
+    10,#Enzyme Action
+    12,#Factors Affecting Enzyme Activity
+    14,
+    16,
+    18,
+    20,
+    22,
+    23,
+    24,
+    28,
+    30,
+    32,
+    34,
+    36,
+    38,
+    40,
+    42,
+    44,
+    46,
+    48,
+    50,
+    52,
+    54,
+    56,
+    58,
+    60,
+    62,
+    64,
+    146,
+    148,
+    152,
+    154,
+    156,
+    159,
+    162,
+    164,
+    166,
+    168,
+    170,
+    172,
+    174,
+    176,
+    178,
+    180,
+    182,
+    184,
+    186,
+    190,
+    193,
+    195,
+    196,
+    199,
+    201,
+    204,
+    206,
+    208,
+    210,
+    213,
+    215,
+    217,
+]
+yes_titles = [
+    'Carbohydrates',
+    'Lipids',
+    'Proteins',
+    'Enzyme Action',
+    'Factors Affecting Enzyme Activity',
+    'Enzyme-Controlled Reactions',
+    'DNA and RNA',
+    'DNA Replication',
+    'Water',
+    'ATP',
+    'Inorganic Ions',
+    'Eukaryotic Cells and Organelles',
+    'Prokaryotic Cells and Viruses',
+    'Analysis of Cell Components',
+    'Cell Division - Mitosis',
+    'Cell Division - Investigating Mitosis',
+    'Cell Membrane Structure',
+    'Exchange Across Cell Membranes - Diffusion',
+    'Exchange Across Cell Membranes - Osmosis',
+    'Exchange Across Cell Membranes - Active Transport',
+    'The Immune System',
+    'Immunity and Vaccines',
+    'Antibodies in Medicine',
+    'Interpreting Vaccine and Antibody Data',
+    'HIV and Viruses',
+    'Size and Surface Area',
+    'Gas Exchange',
+    'Gas Exchange in Humans',
+    'The Effects of Lung Disease',
+    'Interpreting Lung Disease Data',
+    'Dissecting Gas Exchange Systems',
+    'Homeostasis Basics',
+    'Control of Blood Glucose Concentration',
+    'The Kidneys',
+    'Controlling Blood Water Potential',
+    'Inheritance',
+    'Linkage and Epistasis',
+    'The Chi-Squared Test',
+    'The Hardy-Weinberg Principle',
+    'Variation and Selection',
+    'Speciation and Genetic Drift',
+    'Ecosystems',
+    'Variation in Population Size',
+    'Investigating Populations',
+    'Succession',
+    'Conservation',
+    'Mutations',
+    'Cancer',
+    'Interpreting Data on Cancer',
+    'Stem Cells',
+    'Regulation of Transcription and Translation',
+    'Epigenetic Control of Gene Expression',
+    'Evaluating Data on Phenotypes',
+    'Genome Projects and Making DNA Fragments',
+    'Amplifying DNA Fragments',
+    'Using Recombinant DNA Technology',
+    'Gene Probes and Medical Diagnosis',
+    'Gentic Fingerprinting',
+    'Planning an Experiment',
+    'Processing and Presenting Data',
+    'Drawing Conclusions and Evaluating',
+    'How To Do Well in Your Exams',
+    'Answers'
+]
+
+#just minus 1:
+no = [
+    2,
+    2,
+    4,
+    13,
+    14,
+    16,
+    18,
+    20,
+    22,
+    24,
+    26,
+    28,
+    29,
+    30,
+    34,
+    36,
+    38,
+    40,
+    42,
+    44,
+    46,
+    48,
+    50,
+    52,
+    54,
+    56,
+    58,
+    60,
+    62,
+    64,
+    66,
+    68,
+    70,
+    72,
+
+
+
+
+]
+
+no_titles = [
+    'Carbohydrates',
+    'How To Do Well in Your Exams',
+    'Drawing Conclusions and Evaluating',
+    'Carbohydrates',
+    'Lipids',
+    'Proteins',
+    'Enzyme Action',
+    'Factors Affecting Enzyme Activity',
+    'Enzyme-Controlled Reactions',
+    'DNA and RNA',
+    'DNA Replication',
+    'Water',
+    'ATP',
+    'Inorganic Ions',
+    'Eukaryotic Cells and Organelles',
+    'Prokaryotic Cells and Viruses',
+    'Analysis of Cell Components',
+    'Cell Division - Mitosis',
+    'Cell Division - Investigating Mitosis',
+    'Cell Membrane Structure',
+    'Exchange Across Cell Membranes - Diffusion',
+    'Exchange Across Cell Membranes - Osmosis',
+    'Exchange Across Cell Membranes - Active Transport',
+    'The Immune System',
+    'Immunity and Vaccines',
+    'Antibodies in Medicine',
+    'Interpreting Vaccine and Antibody Data',
+    'HIV and Viruses',
+    'Size and Surface Area',
+    'Gas Exchange',
+    'Gas Exchange in Humans',
+    'The Effects of Lung Disease',
+    'Interpreting Lung Disease Data',
+    'Dissecting Gas Exchange Systems',
+
+
+]
+
+#for i in range(0,len(no_titles)):
+#    print(no[i],no_titles[i])
+yes = list(map(lambda x: x+5, yes))
+no = list(map(lambda x: x - 1, no))
+reader = PdfReader(r"../../CGP-AQA-Biology-A-Level-ariadanesh.com_.pdf")
+aqabio_data = []
+for i in range(0,len(yes)):
+    aqabio_data.append((reader.pages[yes[i]].extract_text(),yes_titles[i],'Yes'))
+for i in range(0,len(no)):
+    aqabio_data.append((reader.pages[no[i]].extract_text(),no_titles[i],'No'))
+
+#aqabio_data = [reader.pages[i].extract_text() for i in pages]
+# -----------------------------------------------------------------------------
+
+
+# -----------------------------------------------------------------------------
+
+# %% --------------------------------------------------------------------------
+# cambridge business book
+yes = [#add 7
+    -1,
+    2,3,15,29,38,51,61,70,76,98,124,125,137,159,170,187,200,212,213,231,252,273,297,318,330,331,339,366,376,385,403,416,417,434,443,464,476,486,495,505,518,534,535,543,556,568,587
+]
+yes_titles = ['Introduction',
+              'Unit 1 Business and its environment',
+              'Enterprise',
+              'Business structure',
+              'Size of business',
+              'Business objectives',
+              'Stakeholders in a business',
+              'Business structure (A level)',
+              'Size of a business (A level)',
+              'External influences on business activity (A level)',
+              'External economic influences on business behaviour (A level)',
+              'Unit 2 People in organisations',
+              'Management and leadership',
+              'Motivation',
+              'Human resource management',
+              'Further human resource management (A Level)',
+              'Organisation structure (A Level)',
+              'Business communication (A Level)',
+              'Unit 3 Marketing',
+              'What is marketing?',
+              'Market research',
+              'The marketing mix - product and price',
+              'The marketing mix - promotion and place',
+              'Marketing planning (A Level)',
+              'Globalisation and international marketing (A Level)',
+              'Unit 4 Operations and project management',
+              'The nature of operations',
+              'Operations planning',
+              'Inventory management',
+              'Capacity utilisation (A Level)',
+              'Lean production and quality management (A Level)',
+              'Project management (A Level)',
+              'Unit 5 Finance and accounting',
+              'Business finance',
+              'Costs',
+              'Accounting fundamentals',
+              'Forecasting and managing cash flows',
+              'Costs (A levels)',
+              'Budgets (A Level)',
+              'Contents of published accounts (A Level)',
+              'Analysis of published accounts (A Level)',
+              'Investment appraisal (A Level)',
+              'Unit 6 Strategic management',
+              'What is strategic management? (A Level)',
+              'Strategic analysis (A Level)',
+              'Strategic choice (A Level)',
+              'Strategic implementation (A Level)',
+              'Preparing for your examinations',
+              ]
+
+no = [# minus 1
+    4,4,7,7,8,8,9,10,11,23,37,57,59,72,82,90,111,132#check
+    ,133,145,176,180,195,211,220,221,239,279,281,319,328,338,339,363,374,388,393,423,424,425,449,451,475,493,494,503,514,526,542,543,551,564,576,
+]
+
+no_titles = [
+    'Business structure',
+    'Unit 1 Business and its environment',
+    'Business communication (A Level)',
+    'External influences on business activity (A level)',
+    'Introduction',
+    'Understanding Strategy',
+    'Unit 6 Strategic management',
+    'Enterprise',
+    'Business structure',
+    'Size of business',
+    'Business objectives',
+    'Stakeholders in a business',
+    'Business structure (A level)',
+    'Size of a business (A level)',
+    'External influences on business activity (A level)',
+    'External economic influences on business behaviour (A level)',
+    'Unit 2 People in organisations',
+    'Management and leadership',
+    'Motivation',
+    'Human resource management',
+    'Further human resource management (A Level)',
+    'Organisation structure (A Level)',
+    'Business communication (A Level)',
+    'Unit 3 Marketing',
+    'What is marketing?',
+    'Market research',
+    'The marketing mix - product and price',
+    'The marketing mix - promotion and place',
+    'Marketing planning (A Level)',
+    'Globalisation and international marketing (A Level)',
+    'Unit 4 Operations and project management',
+    'The nature of operations',
+    'Operations planning',
+    'Inventory management',
+    'Capacity utilisation (A Level)',
+    'Lean production and quality management (A Level)',
+    'Project management (A Level)',
+    'Unit 5 Finance and accounting',
+    'Business finance',
+    'Costs',
+    'Accounting fundamentals',
+    'Forecasting and managing cash flows',
+    'Costs (A levels)',
+    'Budgets (A Level)',
+    'Contents of published accounts (A Level)',
+    'Analysis of published accounts (A Level)',
+    'Investment appraisal (A Level)',
+    'Unit 6 Strategic management',
+    'What is strategic management? (A Level)',
+    'Strategic analysis (A Level)',
+    'Strategic choice (A Level)',
+    'Strategic implementation (A Level)',
+    'Preparing for your examinations',
+
+]
+#pages = list(map(lambda x: x - 1, pages))
+#reader = PdfReader(r"../../cambridge business textbook.pdf")
+#biscam_data = [reader.pages[i].extract_text() for i in pages]
+
+for i in range(0,len(no_titles)):
+    print(no[i],no_titles[i])
+
+yes = list(map(lambda x: x+7, yes))
+no = list(map(lambda x: x - 1, no))
+reader = PdfReader(r"../../cambridge business textbook.pdf")
+biscam_data = []
+for i in range(0,len(yes)):
+    biscam_data.append((reader.pages[yes[i]].extract_text(),yes_titles[i],'Yes'))
+for i in range(0,len(no)):
+    biscam_data.append((reader.pages[no[i]].extract_text(),no_titles[i],'No'))
+
+
+
+# %%
+# edexcel biology pearson
+yes = [
+    -4,
+    -2,
+    0, #these 3 diff papges
+    2,4,8,11,14,16,22,26,28,31,37,41,44,50,52,54,56,59,63,65,67,70 #add 9
+]
+yes_titles = [
+    'ABOUT THIS BOOK',
+    'PRACTICAL SKILLS',
+    'ASSESSMENT OVERVIEW',
+    '1A CHEMISTRY FOR BIOLOGISTS',
+    '1 THE CHEMISTRY OF LIFE',
+    '2 CARBOHYDRATES 1: MONOSACCHARIDES AND DISACCHARIDES',
+    '3 CARBOHYDRATES 2: POLYSACCHARIDES',
+    '4 LIPIDS',
+    '5 PROTEINS',
+    'THINKING BIGGER: TREHALOSE',
+    '1B MAMMALIAN TRANSPORT SYSTEMS',
+    '1 THE PRINCIPLES OF CIRCULATION',
+    '2 THE ROLES OF THE BLOOD',
+    '3 CIRCULATION IN THE BLOOD VESSELS',
+    '4 THE MAMMALIAN HEART',
+    '5 ATHEROSCLEROSIS',
+    '1C CARDIOVASCULAR HEALTH AND RISK',
+    '1 RISK, CORRELATION AND CAUSE',
+    '2 INVESTIGATING THE CAUSES OF CVDs',
+    '3 RISK FACTORS FOR CARDIOVASCULAR DISEASE',
+    '4 DIET AND CARDIOVASCULAR HEALTH',
+    '5 DIETARY ANTIOXIDANTS AND CARDIOVASCULAR DISEASE',
+    '6 USING THE EVIDENCE',
+    '7 THE BENEFITS AND RISKS OF TREATMENT',
+    'THINKING BIGGER: HEART FAILURE',
+]
+
+no = [
+    2,2,6,6,7,7,8,9,9,10,10,11,11,
+    12,13,
+    14,18,21,24,26,32,36,40,41,50,53,54,60,62,64,66,71,73,75,79
+]
+
+no_titles = [
+    'ABOUT THIS BOOK',
+    '21 CHEMISTRY',
+    'SPECIFICATION SUMMARY',
+    'INTRODUCTION',
+    'THINKING BIGGER: ENZYMES',
+    '2 THE ROLES OF THE BLOOD',
+    'ASSESSMENT OVERVIEW',
+    '1 THE CELL WALL',
+    '1A CHEMISTRY FOR BIOLOGISTS',
+    'STATISTICS',
+    '1 THE CHEMISTRY OF LIFE',
+    'CARBOHYDRATES',
+    'DATA AND METHODS',
+    '1 THE CHEMISTRY OF LIFE',
+    'IONIC AND COVALENT BONDING',
+    '2 CARBOHYDRATES 1: MONOSACCHARIDES AND DISACCHARIDES',
+    '3 CARBOHYDRATES 2: POLYSACCHARIDES',
+    '4 LIPIDS',
+    '5 PROTEINS',
+    'THINKING BIGGER: TREHALOSE',
+    '1B MAMMALIAN TRANSPORT SYSTEMS',
+    '1 THE PRINCIPLES OF CIRCULATION',
+    '2 THE ROLES OF THE BLOOD',
+    '3 CIRCULATION IN THE BLOOD VESSELS',
+    '4 THE MAMMALIAN HEART',
+    '5 ATHEROSCLEROSIS',
+    '1C CARDIOVASCULAR HEALTH AND RISK',
+    '1 RISK, CORRELATION AND CAUSE',
+    '2 INVESTIGATING THE CAUSES OF CVDs',
+    '3 RISK FACTORS FOR CARDIOVASCULAR DISEASE',
+    '4 DIET AND CARDIOVASCULAR HEALTH',
+    '5 DIETARY ANTIOXIDANTS AND CARDIOVASCULAR DISEASE',
+    '6 USING THE EVIDENCE',
+    '7 THE BENEFITS AND RISKS OF TREATMENT',
+    'THINKING BIGGER: HEART FAILURE',
+
+
+]
+
+yes = list(map(lambda x: x+9, yes))
+no = list(map(lambda x: x - 1, no))
+reader = PdfReader(r"../../biology textbook.pdf")
+edbio_data = []
+for i in range(0,len(yes)):
+    edbio_data.append((reader.pages[yes[i]].extract_text(),yes_titles[i],'Yes'))
+for i in range(0,len(no)):
+    edbio_data.append((reader.pages[no[i]].extract_text(),no_titles[i],'No'))
+
+# %% --------------------------------------------------------------------------
+# hodder
+no = [
+    2,2,2,3,4,4,7 #minus 1
+]
+no_titles = [
+    'Introduction',
+    '1 - Cell division',
+    'Cell biology',
+    'Biological systems',
+    'Cell membranes',
+    'Topic 1 - Gas',
+    'bacteria'
+
+]
+no = list(map(lambda x: x - 1, no))
+reader = PdfReader(r"../../AQA-8461-HODDER-SAMPLE.pdf")
+hod_data = []
+for i in range(0,len(no)):
+    hod_data.append((reader.pages[no[i]].extract_text(),no_titles[i],'No'))
+
+# %% --------------------------------------------------------------------------
+
+combi = aqabio_data+biscam_data+edbio_data+hod_data
+final = pd.DataFrame(combi, columns=['page', 'chapter_title', 'label'])
+final.to_json('page_identification_data.json')
+
+
+# %%
+from transformers import AutoTokenizer
+model_id = "TheBloke/Mistral-7B-v0.1-GPTQ"
+
+tokenizer = AutoTokenizer.from_pretrained(
+    model_id, use_fast=False, add_eos_token=True
+)
+# %%
+#hi = aqabio_data[0] +aqabio_data[1]+aqabio_data[2]+aqabio_data[3]
+#hi = biotext_data[11]+biotext_data[12]+biotext_data[13]+biotext_data[14]+biotext_data[15]
+lens = []
+for i in final:
+    lens.append(len(tokenizer(i)['input_ids']))
+
+
+# %%
+plz = pd.read_json('synthetic_data_newest.json')
+for i in range(0,len(plz)):
+    if len(tokenizer(plz['pages'][i])['input_ids']) >= len(tokenizer(plz['sums'][i])['input_ids'])/0.6:
+        print(i)
+
+
+# %% --------------------------------------------------------------------------
+plz['sums'][141]
+# -----------------------------------------------------------------------------
+
+# %%
+#for test ...
+pages = [
+    206,224,226,266,
+    271,272
+]
+pages = list(map(lambda x: x - 1, pages))
+reader = PdfReader(r"../../edexcel_a_level_physics_student_book_1.pdf")
+physics = [reader.pages[i].extract_text() for i in pages]
+e = physics[4]+physics[5]
+physics=physics[0:4]
+physics.append(e)
+pd.DataFrame({'test_set':physics}).to_csv('test_data.csv')
+# %%
+
