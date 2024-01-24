467864aeaa31120f016bb74835e7b60951f7d0c4
seatond1999 <danseaton@kubrickgroup.com>
2024-01-24 08:14:19 +0000

    f

--- revamp/firstpage_classhead_eval_memorysaving.py
+++ revamp/firstpage_classhead_eval_memorysaving.py
@@ -9,6 +9,7 @@ from transformers import (
     AutoModelForCausalLM,AutoModelForSequenceClassification
 )
 import torch
+from torch import nn
 from openai import OpenAI
 import openai
 import os
@@ -123,15 +124,6 @@ def metrics(df):
     return recall,specificity
 # -----------------------------------------------------------------------------
 
-# %% --------------------------------------------------------------------------
-model.num_labels = 2
-model.config.num_labels = 2
-model.score = nn.Linear(model.config.hidden_size, model.config.num_labels, bias=False)
-
-model.lm_head = None
-model.config.lm_head = None #remove mpapping to vocab size
-#removing above heads nut in actual thing would save them and then put them back for causal bits and remove score head
-model.conditioner = 'causal'
 # -----------------------------------------------------------------------------
 
 
@@ -140,69 +132,40 @@ if __name__ == "__main__":
     adapter_path = "seatond/multi_yes_short"
     base_model_path = "TheBloke/Mistral-7B-Instruct-v0.2-GPTQ"
     tokenizer, model = load_model(adapter_path, base_model_path)
-    test_data_path = r"firstpage_testdata.json"
-    test_data = prep_data(test_data_path)
-    results = inference(tokenizer,model,test_data)
-    recall, specificity = metrics(results)
-
+    #test_data_path = r"firstpage_testdata.json"
+    #test_data = prep_data(test_data_path)
+    #results = inference(tokenizer,model,test_data)
+    #recall, specificity = metrics(results)
 
 
 # %% --------------------------------------------------------------------------
-#confusion matrix and recall and specificity
-
-df = pd.DataFrame({'label': ['yes', 'no', 'yes', 'no', 'yes'],'prediction': ['yes', 'no', 'yes', 'yes', 'yes']})
-conf_matrix = confusion_matrix(df['label'], df['prediction'])
-conf_matrix_df = pd.DataFrame(conf_matrix, index=['Actual No', 'Actual Yes'], columns=['Predicted No', 'Predicted Yes'])
-
-# Plot the confusion matrix using seaborn heatmap
-plt.imshow(conf_matrix, interpolation='nearest', cmap=plt.cm.Blues)
-plt.title('Confusion Matrix')
-plt.colorbar()
-
-classes = ['No', 'Yes']
-tick_marks = range(len(classes))
-plt.xticks(tick_marks, classes)
-plt.yticks(tick_marks, classes)
-
-plt.xlabel('Predicted Label')
-plt.ylabel('True Label')
-for i in range(len(classes)):
-    for j in range(len(classes)):
-        plt.text(j, i, str(conf_matrix[i, j]), ha='center', va='center', color='black', fontsize=14)
-plt.show()
-
-recall = conf_matrix[1, 1] / (conf_matrix[1, 1] + conf_matrix[1, 0]) #check these calcs are right
-specificity = conf_matrix[0, 0] / (conf_matrix[0, 0] + conf_matrix[0, 1])
+model.num_labels = 2
+model.config.num_labels = 2
+model.score = nn.Linear(model.config.hidden_size, model.config.num_labels, bias=False,dtype=torch.float16)
+model.lm_head = None
+model.config.lm_head = None #remove mpapping to vocab size
+#removing above heads nut in actual thing would save them and then put them back for causal bits and remove score head
 # -----------------------------------------------------------------------------
 
-# %%
-adapter_path = "/home/seatond/revision_project/revamp/firstpage_rank32_lr2.2e-05_target7_epochs1_laplha64_batch2_gradacc2"
-base_model_path = "TheBloke/Mistral-7B-Instruct-v0.2-GPTQ"
-tokenizer, model = load_model(adapter_path, base_model_path)
-test_data_path = r"firstpage_testdata.json"
-test_data = prep_data(test_data_path)
-# %%
-ground = []
-pred = []
-for i in range(0,38):
-    print(i)
-    X_test_tokenized = tokenizer(test_data['content'][i], return_tensors="pt").input_ids.to("cuda:0")
-    with torch.no_grad():
-        logits = model(X_test_tokenized).logits
+# %% --------------------------------------------------------------------------
+model.conditioner = 'classification'
+print(model.score)
 
-    # Convert logits to probabilities using softmax
-    probs = torch.nn.functional.softmax(logits, dim=-1)
+# %% --------------------------------------------------------------------------
+input_ids = tokenizer('hello there', return_tensors="pt").input_ids.to("cuda:0")
 
-    # Get the predicted class index
-    predicted_class = torch.argmax(probs).item()
-    predicted_class = 'yes' if predicted_class == 1 else 'No'
+model = model.to("cuda:0")
 
-    pred.append(predicted_class)
-    ground.append(test_data['label'][i])
-    print(i)
+# Move input tensors to GPU
+input_ids = input_ids.to("cuda:0")
 
-# %%
-results = pd.DataFrame({'ground':ground,'pred':pred})
-results
+with torch.no_grad():
+    logits = model(input_ids)
 
-# %%
+probs = torch.nn.functional.softmax(logits, dim=-1)
+
+# Get the predicted class index
+predicted_class = torch.argmax(probs).item()
+predicted_class = 'yes' if predicted_class == 1 else 'No'
+# -----------------------------------------------------------------------------
+
